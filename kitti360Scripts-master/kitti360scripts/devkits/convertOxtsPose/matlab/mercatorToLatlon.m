82ea15296ce746bcbd2af9c60fcd7417f93dfac0
Jialin <tjyhleo@outlook.com>
2022-08-22 08:35:41 +0800

    update

--- kitti360Scripts-master/kitti360scripts/devkits/convertOxtsPose/matlab/mercatorToLatlon.m
+++
@@ -1,6 +0,0 @@
-function [lat,lon] = mercatorToLatlon(mx,my,scale)
-% converts mercator coordinates using mercator scale to lat/lon coordinates
-
-er = 6378137; % average earth radius at the equator
-lon = mx * 180 / (scale * pi * er); 
-lat = 360 / pi * atan(exp(my / (scale * er))) - 90;
--- kitti360Scripts-master/kitti360scripts/viewer/testing.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-a=np.array([[2,2],[2,2]])
-b=np.array([[1,2],[3,4]])
-print(np.matmul(a,b))
